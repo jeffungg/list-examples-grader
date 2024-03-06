6a0a8ebc7abd6efad6d9a49f312c72f3cbe1c1ad
jeffungg <jeffung2003@gmail.com>
2024-03-06 12:09:26 -0800

    grade.sh updated

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,40 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+    echo "ListExamples.java copied to grading-area"
+else 
+    echo "ListExamples.java missing"
+fi
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then 
+    echo "The program failed to compile, see errors above"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+if [[ $(head -n 2 junit-output.txt | tail -n 1 | grep "E") != "" ]]
+then
+    lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+    tests=$(echo $lastline | awk -F '[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F '[, ]' '{print $6}')
+    successes=$((tests-failures))
+    echo "Failed tests: "
+    grep ".) test" junit-output.txt
+    echo "Number of tests passed: $successes / $tests"
+else
+    lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+    numtests=$(echo $lastline | grep -o '[0-9]*' | head -1)
+    echo "All tests passed"
+    echo "Number of tests passed: $numtests / $numtests"
+fi
